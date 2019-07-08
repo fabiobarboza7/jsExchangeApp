c062099c9090765bf1aee71528dac9530e4d9119
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-08 20:42:30 -0300

    ajax/json api done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 // jQuery
 $(document).ready(function(){
+	const api = 'https://currencydatafeed.com/api/data.php?token=q16wrubwfnpannjdgd1x&currency=USD/BRL';
+	const botao = $('button');
+	let resultado = $('#resultado');
+
+	botao.click(function(){
+		let valor = $('#valor').val();
+
+		$.getJSON(api, function(data){
+			let val = data.currency[0].value; 
+			let calculo = val*valor
+			resultado.html(`<p>${calculo.toFixed(2)}</p>`);
+		});
+	});
 
 });
 // end jQuery
